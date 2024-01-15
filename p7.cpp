fd4124baf59eda7fa8a8b8e343b637c175faa9c5
MuhammadHassan05 <156007695+MuhammadHassan05@users.noreply.github.com>
2024-01-16 00:26:02 +0500

    Add files via upload

---
+++ p7.cpp
@@ -0,0 +1,27 @@
+//#include<iostream>
+//using namespace std;
+//int main() {
+//	int marks;
+//	cout << "Enter marks between 0 to 100 " << endl;
+//	cin >> marks;
+//	switch (marks) {
+//	case 1:
+//		cout << "marks >=90 && marks <=100";
+//		cout << "Grade is A";
+//		break;
+//	case 2:
+//		cout << "marks >=75 && marks <=90";
+//		cout << "Grade is B";
+//		break;
+//	case 3:
+//		cout << "marks >=60 && marks <= 75";
+//		cout << "Grade is C";
+//		break;
+//	case 4:
+//		cout << "marks >=45 && marks <=60";
+//		cout << "Grade is D";
+//		break;
+//	default:
+//		cout << "fail ";
+//	}
+//}
